060b4392f66f62d1e5794bbfcaeac1ca2135f89c
AmulyaReddy99 <amulyareddyk@yahoo.in>
2019-06-01 20:55:53 +0530

    begin_end page numbers

--- table.py
+++ table.py
@@ -13,13 +13,23 @@ def fnPDF_FindText(xFile, xString):
 			PageFound = i
 			content = content.replace('.','')
 			at = content.index(result.group())+len(result.group())
-			return int(content[at+2:at+5])
+			begin = int(content[at+2:at+5])
+			# iterate till next number
+			i,end = 7,0
+			while True:
+				if content[at+i].isdigit():
+					while(content[at+i : at+end+i+1].isdigit()):
+						end+=1
+					end = int(content[at+i:at+end+i+1])
+					break
+				i+=1
+			return begin, end
 
 
 xFile = 'test1.pdf'
 test = 'test1'
 xString = r'summary(.{4}| )financial (?:information|statement)'
-page_num = fnPDF_FindText(xFile, xString)
+page_num, _ = fnPDF_FindText(xFile, xString)
 print(page_num)
 
 tabula.convert_into(xFile, (test + '.csv'), output_format="csv", pages=page_num+1)
--- text_summary.py
+++ text_summary.py
@@ -1,12 +1,41 @@
  
 import re, nltk, PyPDF2
+# from table import fnPDF_FindText
 
-xFile = 'test1.pdf'
+def fnPDF_FindText(xFile, xString):
+	import PyPDF2, re
+	PageFound = -1
+	pdfDoc = PyPDF2.PdfFileReader(open(xFile, "rb"))
+	for i in range(0, pdfDoc.numPages):
+		content = ""
+		content += pdfDoc.getPage(i).extractText().strip().replace('\n','').lower()
+		result = re.search(xString, content)
+		print(i, result)
+		if result is not None:
+			PageFound = i
+			content = content.replace('.','')
+			at = content.index(result.group())+len(result.group())
+			begin = int(content[at+2:at+5])
+			# iterate till next number
+			i,end = 7,0
+			while True:
+				if content[at+i].isdigit():
+					while(content[at+i : at+end+i+1].isdigit()):
+						end+=1
+					end = int(content[at+i:at+end+i+1])
+					break
+				i+=1
+			return begin, end
+
+xFile = 'test2.pdf'
+xString = r'our business'
 pdfDoc = PyPDF2.PdfFileReader(open(xFile, "rb"))
 
 article_text = ""
 
-for i in range(113, 126):
+begin, end = fnPDF_FindText(xFile, xString)
+print("--:",begin,end)
+for i in range(begin, end):
 	article_text += pdfDoc.getPage(i).extractText().strip().replace('\n','')
 
 
@@ -44,3 +73,8 @@ summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
 
 summary = ' '.join(summary_sentences)  
 print(summary) 
+
+with open('summary.txt','a') as file:  
+    file.write(summary+'\n')
+
+
